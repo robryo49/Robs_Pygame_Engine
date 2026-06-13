92dfd77aca9a2095df6b8abe594050685d6d143c
robin <robin.ryobouchereau@yahoo.com>
2026-06-13 19:01:41 +0200

    fixed matplotlib error with cmaps

--- src/robs_pge/utils/image_processing.py
+++ src/robs_pge/utils/image_processing.py
@@ -1,14 +1,14 @@
-import logging
 from math import cos, pi, sin
 from random import randrange
+from typing import Optional
 
 import noise
 import numpy as np
-#from matplotlib.cm import get_cmap TODO fix get_cmap
+from matplotlib import colormaps
 from matplotlib.colors import Colormap
 from pygame import Vector2 as Vec2
 
-from src.robs_pge.utils import invert_uv_y
+from .math import invert_uv_y
 
 
 def normalize_array(arr: np.ndarray, min_v: float, max_v:float):
@@ -16,12 +16,11 @@ def normalize_array(arr: np.ndarray, min_v: float, max_v:float):
     return (arr - arr_min) * (max_v - min_v) / (arr_max - arr_min) + min_v
 
 
-def colorize_array(arr: np.ndarray, cmap: str | Colormap="binary"):
-    logging.warning("colorize array fix Not implemented yet")
-    return arr #get_cmap(cmap)(arr) * 255
+def colorize_array(arr: np.ndarray, cmap: str | Colormap = "binary"):
+    return colormaps.get_cmap(cmap)(arr) * 255
 
 
-def noise_array(dims: Vec2, seed: int=None, scale: float=100, amplitude: float=1, offset: float=0, octaves: int=8, persistence: float=0.5, lacunarity: float=2.0):
+def noise_array(dims: Vec2, seed: Optional[int]=None, scale: float=100, amplitude: float=1, offset: float=0, octaves: int=8, persistence: float=0.5, lacunarity: float=2.0):
     
     seed = randrange(10000) if seed is None else seed
     width, height = int(dims.x), int(dims.y)
@@ -42,19 +41,19 @@ def noise_array(dims: Vec2, seed: int=None, scale: float=100, amplitude: float=1
     return arr * amplitude + (offset + 0.5)
 
 
-def rotated_surface_dims(dims, rotation):
+def rotated_surface_dims(dims: Vec2, rotation: float):
     return Vec2(
         abs(dims.x * cos(rotation*pi/180)) + abs(dims.y * sin(rotation*pi/180)),
         abs(dims.y * cos(rotation*pi/180)) + abs(dims.x * sin(rotation*pi/180))
     )
 
 
-def surface_pos_from_pixel_pos(pixel_pos: Vec2, dims: Vec2, rotation=0):
+def surface_pos_from_pixel_pos(pixel_pos: Vec2, dims: Vec2, rotation: float=0):
     if rotation:
         return ((pixel_pos - dims/2).rotate(-rotation) + rotated_surface_dims(dims, rotation) / 2).elementwise()
     else:
         return pixel_pos
 
 
-def surface_pos_from_uv_pos(uv: Vec2, dims: Vec2, rotation=0):
+def surface_pos_from_uv_pos(uv: Vec2, dims: Vec2, rotation: float=0):
     return surface_pos_from_pixel_pos(invert_uv_y(uv).elementwise() * dims, dims, rotation)
