a77666f1cc41e22b9607c554ac4e4b9df38fbf51
robin <robin.ryobouchereau@yahoo.com>
2026-07-12 17:14:32 +0200

    added with_alpha to object Styles

--- src/robs_pge/rendering/styles.py
+++ src/robs_pge/rendering/styles.py
@@ -5,7 +5,7 @@ from ..utils import Color, Font, Vec2, Colors
 @dataclass
 class Style:
     def with_(self, **kwargs):
-        return replace(self, **kwargs)
+        return replace(self, **kwargs)  # type: ignore[arg-type]
     
     def copy(self):
         return replace(self)
@@ -19,6 +19,14 @@ class ShapeStyle(Style):
     bg_color: Color = field(default_factory=lambda: Color(255, 255, 255))
     bd: int = 0
     bd_color: Color = field(default_factory=lambda: Color(0, 0, 0))
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            bg_color = Colors.with_alpha(self.bg_color, alpha),
+            bd_color = Colors.with_alpha(self.bd_color, alpha)
+        )
+        
+        
 
 
 @dataclass
@@ -40,7 +48,11 @@ class PolygonStyle(ShapeStyle):
 class LineStyle(Style):
     color: Color = field(default_factory=lambda: Color(255, 255, 255))
     width: int = 1
-
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            color = Colors.with_alpha(self.color, alpha),
+        )
 
 # endregion
 
@@ -50,30 +62,57 @@ class ButtonStyle(Style):
     bg_style: RectStyle = field(default_factory=RectStyle)
     margin: int = 50
     font: Font = field(default_factory=Font)
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            bg_style = self.bg_style.with_alpha(alpha),
+        )
 
 
 @dataclass
 class SpriteButtonStyle(Style):
     bg_style: RectStyle = field(default_factory=RectStyle)
     margin: int = 10
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            bg_style = self.bg_style.with_alpha(alpha),
+        )
 
 
 @dataclass
 class IconButtonStyle(Style):
     button_style: SpriteButtonStyle = field(default_factory=SpriteButtonStyle)
     icon_color: Color = field(default_factory=lambda: Color(255, 255, 255))
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            button_style = self.button_style.with_alpha(alpha),
+            icon_color = Colors.with_alpha(self.icon_color, alpha),
+        )
 
 @dataclass
-class ToggleButtonStyle:
+class ToggleButtonStyle(Style):
     bg_style: RectStyle = field(default_factory=RectStyle)
     toggle_style: RectStyle = field(default_factory=RectStyle)
     toggle_bg_color: Color = field(default_factory=lambda: Color(0, 0, 0))
-
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            bg_style = self.bg_style.with_alpha(alpha),
+            toggle_bg_color = Colors.with_alpha(self.toggle_bg_color, alpha),
+        )
 
 @dataclass
 class ProgressBarStyle(Style):
     bg_style: RectStyle = field(default_factory=RectStyle)
     color: Color = field(default_factory=lambda: Color(255, 255, 255))
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            bg_style = self.bg_style.with_alpha(alpha),
+            color = Colors.with_alpha(self.color, alpha),
+        )
 
 
 @dataclass
@@ -81,6 +120,12 @@ class LineChartStyle(Style):
     bg_style: RectStyle = field(default_factory=RectStyle)
     line_color: Color = field(default_factory=lambda: Color(255, 255, 255))
     line_width: int = 1
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            bg_style = self.bg_style.with_alpha(alpha),
+            line_color = Colors.with_alpha(self.line_color, alpha),
+        )
 
 
 @dataclass
@@ -93,6 +138,13 @@ class SliderStyle(Style):
     font: Font = field(default_factory=Font)
     text_position: str = "right"
     hide_bg: bool = False
+    
+    def with_alpha(self, alpha: int):
+        return self.with_(
+            bg_style = self.bg_style.with_alpha(alpha),
+            bar_style = self.bar_style.with_alpha(alpha),
+            handle_style = self.handle_style.with_alpha(alpha),
+        )
 
 
 @dataclass
