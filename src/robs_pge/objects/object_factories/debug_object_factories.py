af9798490fa6eeae69d1f1143572e902435c0caa
robin <robin.ryobouchereau@yahoo.com>
2026-08-08 19:20:39 +0200

    bug fix

--- src/robs_pge/objects/object_factories/debug_object_factories.py
+++ src/robs_pge/objects/object_factories/debug_object_factories.py
@@ -8,49 +8,6 @@ from ...utils import Anchor, Transform, inf, vec2
 
 class DebugObjectFactory(SubObjectFactory):
     
-    def make_debug_panel(
-            self, position: vec2, dims: vec2, style: DebugPanelStyle | str, title: str,
-            rotation: float = 0.0, scale: float = 1.0, layer: int = 0,
-            anchor: vec2 = Anchor.C, cache: bool = True
-    ) -> DebugPanelObject:
-        
-        style = self._get_resource(style, DebugPanelStyle)
-        
-        width = round(dims.x)
-        
-        header_height = 4
-        title_height = 30
-        panel_height = round(dims.y - title_height - header_height)
-        
-        panel = self.factory.ui.layouts.make_grid_layout(vec2(), width, panel_height, style=style.panel_style)
-        title_panel = self.factory.shape.make_rect(vec2(), vec2(width, title_height), style=style.title_panel_style)
-        header = self.factory.shape.make_rect(vec2(), vec2(width, header_height), style=style.header_style)
-        title_text = self.factory.text.make_text(vec2(8, -1), title.upper(), style.title_font, anchor=Anchor.L)
-        
-        title_panel.add_child(title_text, Anchor.L)
-        
-        layout = DebugPanelObject(
-            Transform(position, rotation, scale),
-            RectRenderer(vec2(), None, cache),
-            self.factory.services,
-            panel,
-            title_panel,
-            header,
-            title_text,
-            layer,
-            anchor
-        ).skip_rendering()
-        
-        layout.fix_width(width)
-        layout.fix_height(round(dims.y))
-        
-        layout.stack_y(panel)
-        layout.stack_y(title_panel)
-        layout.stack_y(header)
-        
-        return layout
-    
-    
     def make_debug_overlay(
             self, position: vec2, width: Optional[float] = None, height: Optional[float] = None, min_col=0, max_col=inf, min_row=0, max_row=inf, invert_x=False, invert_y=False,
             rotation: float = 0.0, scale: float = 1.0, layer: int = 0, anchor: vec2 = Anchor.C, cache: bool=True
